ef1b54c10e416800895be7d292ca6a33ffb20d82
fehis <fabian.ehisehiabhi@gmail.com>
2023-07-10 00:47:57 +0200

    HTML and functionality added

---
+++ scripts/app.js
@@ -0,0 +1,71 @@
+const setup = () => {
+    
+    let button = document.getElementById('submitButton');
+
+    button.addEventListener('click', () => {
+        //clearing word
+        for(let child of document.getElementById('content').children) {
+            child.textContent = '';
+        }
+        displayWord();
+    });
+}
+
+
+const searchWord = async word => {
+
+    try {
+        let response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word);
+        let data = await response.json();
+        return data[0]
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const displayWord = async () => {
+
+    let word = document.getElementById('word');
+    let phonetic = document.getElementById('phonetic');
+    let definition = document.getElementById('definition');
+
+    let data = await searchWord(document.getElementById('input').value);
+
+    //displaying word
+    word.textContent = data.word
+
+    //displaying phonetic
+    let i = 0;
+    while(i < data.phonetics.length) {
+        if(data.phonetics[i].text && data.phonetics[i].audio) {
+            phonetic.textContent = data.phonetics[i].text;
+            phonetic.addEventListener('click', () => {
+                playPhoneticAudio(i);
+            });
+            break;
+        }
+        i++;
+    }
+
+    //displaying definitions
+    for(let meaning of data.meanings) {
+        let span = document.createElement('span');
+        let partOfSpeech = document.createTextNode(meaning.partOfSpeech);
+        let definitionText = document.createTextNode(meaning.definitions[Math.floor(Math.random() * meaning.definitions.length)].definition);
+
+        span.appendChild(partOfSpeech);
+        definition.appendChild(span);
+        definition.appendChild(document.createElement('br'));
+        definition.appendChild(definitionText);
+        definition.appendChild(document.createElement('br'));
+    }
+
+}
+
+const playPhoneticAudio = async (index) => {
+
+    let data = await searchWord(document.getElementById('input').value);
+    new Audio(data.phonetics[index].audio).play();
+}
+
+window.addEventListener('load', setup);
